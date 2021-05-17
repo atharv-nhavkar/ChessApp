55bf8aff07286c49552406ebfce8cc986009a705
Shri <bhatshripad18@gmail.com>
2021-05-17 18:26:09 +0530

    First Commit

---
+++ app/src/main/java/com/example/chessapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.chessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseReference databaseReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        databaseReference= FirebaseDatabase.getInstance().getReference("Users");
+        databaseReference.child("magnus").setValue("GM");
+    }
+}
